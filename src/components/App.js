7a76226e83af5b0ebfdc3c6340cff6681315a4c3
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-05-19 19:55:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Amazon Clone</h1>
+      <p data-ns-test="project-description">Amazon clone in which user can register and login, see the products list, add to cart and remove from the cart when required</p>
+    </div>
   )
 }
 
