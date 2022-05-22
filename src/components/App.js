b953f5b7dcae358c7cc8acfc2317d948b31f9353
surbhi322 <81092552+surbhi322@users.noreply.github.com>
2022-05-22 22:38:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test={project-name}>Amazon Clone</h1>
-      <p data-ns-test={project-description}>Amazon clone in which user can register and login, see the products list, add to cart and remove from the cart when required</p>
+      <h1 data-ns-test="project-name">Amazon Clone</h1>
+      <p data-ns-test="project-description">Amazon clone in which user can register and login, see the products list, add to cart and remove from the cart when required</p>
     </div>
   )
 }
